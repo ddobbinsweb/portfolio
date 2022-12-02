40c6cb3f5fa9be396a9283d6785ea905e5a7a98c
David Dobbins <ddobbinsweb@gmail.com>
2022-12-02 17:45:01 -0500

    create header and hero

---
+++ components/Hero.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+type Props = {};
+
+export default function Hero({}: Props) {
+  const [text, helper] = useTypewriter({
+    words: ["Yo, I'm Dobbins nice of you to stop by"],
+    loop: false,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      {/* use nextjs images */}
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://images.unsplash.com/photo-1668603145974-c05f7a0e4552?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
+        alt=""
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Senior Software Engineer
+        </h2>
+
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A"></Cursor>
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+
+          <Link href="#contact">
+            <button className="heroButton">Contact</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
