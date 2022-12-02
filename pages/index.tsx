144d6af769b762f6789cd8004925a62aae3c654c
David Dobbins <ddobbinsweb@gmail.com>
2022-12-02 17:48:30 -0500

    add about

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
@@ -18,6 +19,7 @@ export default function Home() {
       </section>
 
       {/* About */}
+      <About />
       {/* Experience */}
       {/* Skills */}
       {/* Projects */}
