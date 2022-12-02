345fc66fee55f352d508c74f1b48f7a83d45f543
David Dobbins <ddobbinsweb@gmail.com>
2022-12-02 18:40:58 -0500

    formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,21 +7,17 @@ export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
-        <title>David Portfolio</title>
+        <title>David`&apos;`s Portfolio</title>
         <meta name="description" content="David Dobbins Portfolio" />
       </Head>
-
       <Header />
-
       {/* Hero */}
       <section id="hero" className="snap-center">
         <Hero />
       </section>
-
       {/* About */}
       <section id="hero" className="snap-center">
-
-      <About />
+        <About />
       </section>
       {/* Experience */}
       {/* Skills */}
