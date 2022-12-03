d4b293a8bf6a872449cc3f7a78a3d4b0e57b8970
David Dobbins <ddobbinsweb@gmail.com>
2022-12-02 23:01:48 -0500

    adding content to about

--- components/About.tsx
+++ components/About.tsx
@@ -28,8 +28,35 @@ export default function About({}: Props) {
       />
 
       <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text=4xl font-semibold">Here is a <span className="underline decoration-[#0ea5e9]">little</span> background</h4>
+        <h4 className="text=4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#0ea5e9]">little</span>{" "}
+          background
+        </h4>
       </div>
+      <p className="text-base">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non
+        massa rutrum, auctor mauris ut, porttitor nibh. Vestibulum pulvinar,
+        magna nec pretium malesuada, lacus diam tincidunt elit, non feugiat est
+        nibh id erat. Praesent et laoreet arcu. Fusce tempus a leo cursus
+        faucibus. Aliquam tempus mollis ligula vel iaculis. Suspendisse potenti.
+        Etiam nibh ante, blandit suscipit orci eu, suscipit malesuada felis.
+        Aenean nec ullamcorper quam, eu venenatis felis. Nunc fringilla libero
+        diam, vel ullamcorper metus blandit et. Vivamus porta dapibus eros, in
+        eleifend turpis luctus quis. Fusce gravida augue vitae mi lobortis, vel
+        faucibus libero venenatis. Aliquam laoreet elit et accumsan scelerisque.
+        Nunc sit amet rutrum tellus. Nam eu semper sem. Fusce vestibulum mattis
+        ex ac lobortis. Cras rhoncus ligula pellentesque ipsum lobortis semper.
+        Etiam malesuada dolor velit, quis semper nibh tristique eget. Praesent a
+        enim velit. Donec id faucibus mauris. Interdum et malesuada fames ac
+        ante ipsum primis in faucibus. Nam odio odio, tincidunt ac sapien vitae,
+        eleifend imperdiet neque. Proin ultricies accumsan arcu, vel congue sem
+        placerat quis. Phasellus luctus iaculis libero, a dignissim quam
+        interdum ut. Cras scelerisque, ante vel semper pharetra, est leo sodales
+        mauris, ac pellentesque lacus turpis a ante. Suspendisse ut maximus
+        metus. Nulla nibh tellus, cursus et magna sit amet, feugiat volutpat
+        nisi.
+      </p>
     </div>
   );
 }
