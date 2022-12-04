d1014854c9bbc2613178f43efe6181bffc6a0eed
David Dobbins <ddobbinsweb@gmail.com>
2022-12-03 22:06:08 -0500

    add projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import WorkExperience from "../components/WorkExperience";
 
@@ -13,23 +14,21 @@ export default function Home() {
         <meta name="description" content="David Dobbins Portfolio" />
       </Head>
       <Header />
-      {/* Hero */}
       <section id="hero" className="snap-start">
         <Hero />
       </section>
-      {/* About */}
       <section id="about" className="snap-center">
         <About />
       </section>
-      {/* Experience */}
       <section id="experience" className="snap-center">
         <WorkExperience />
       </section>
-      {/* Skills */}
       <section id="skills" className="snap-start">
         <Skills />
       </section>
-      {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
       {/* Contact Me */}
     </div>
   );
