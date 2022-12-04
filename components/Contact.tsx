a1dd396b13d627944f5dd011d9664880c2b162f8
David Dobbins <ddobbinsweb@gmail.com>
2022-12-03 23:28:44 -0500

    Contact page

---
+++ components/Contact.tsx
@@ -0,0 +1,82 @@
+import { EnvelopeIcon, MapPinIcon } from "@heroicons/react/24/solid";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+type Props = {};
+
+export default function Contact({}: Props) {
+  const {
+    register,
+    handleSubmit,
+    reset
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href=`mailto:me@ddobbins.io?subject=${formData.subject}&body=${formData.message}`;
+   reset();
+  };
+  
+  return (
+    <div
+      className="h-screen flex relative flex-col text-center md:text-left md:flex-row 
+    max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="sectionHeader">Contact</h3>
+
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
+          Suh <span className="decoration-[#0ea5e9]/50 underline"> Dude! </span>
+        </h4>
+
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon
+              className="text-[#0ea5e9] h-7 w-7"
+              title="Call Me"
+              width={"50px"}
+            />
+            <p className="text-2xl">me@ddobins.io</p>
+          </div>
+
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon
+              className="text-[#0ea5e9] h-7 w-7"
+              title="Call Me"
+              width={"50px"}
+            />
+            <p className="text-2xl">Raleigh, NC</p>
+          </div>
+        </div>
+        <form 
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input {...register('name')} placeholder="Name" className="contactInput" type="text" />
+            <input {...register('email')}placeholder="Email" className="contactInput" type="email" />
+          </div>
+
+          <input
+            {...register('subject')}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+
+          <textarea   {...register('message')} placeholder="Message" className="contactInput" />
+          <button
+            type="submit"
+            className="bg-[#0ea5e9] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
