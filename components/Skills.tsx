693764dcbeeae22b042705e70eb0dd024ab66553
David Dobbins <ddobbinsweb@gmail.com>
2022-12-06 22:58:27 -0500

    add null checks

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -21,10 +21,10 @@ export default function Skills({ skills }: Props) {
       </h3>
 
       <div className="grid grid-cols-4 gap-5">
-        {skills?.slice(0, skills.length / 2)?.map((skill, i) => (
+        {skills?.slice(0, skills?.length / 2)?.map((skill, i) => (
           <SkillIcon key={skill?._id} skill={skill} directionLeft />
         ))}
-        {skills?.slice(skills.length / 2, skills.length).map((skill, i) => (
+        {skills?.slice(skills?.length / 2, skills?.length)?.map((skill, i) => (
           <SkillIcon key={skill?._id} skill={skill} />
         ))}
       </div>
