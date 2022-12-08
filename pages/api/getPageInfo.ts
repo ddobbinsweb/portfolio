710988f44a89682d9ac62d44db7a16dcc8ece01f
David Dobbins <ddobbinsweb@gmail.com>
2022-12-07 21:55:38 -0500

    log query

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -16,6 +16,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log(query);
   const pageInfo: PageInfo = await sanityClient.fetch(query);
   res.status(200).json({ pageInfo });
 }
